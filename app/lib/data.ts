2956aee9a35b488f50cecfc9b45975f75adbf442
Rafael-Machado01 <rafael.machado.ampudia01@gmail.com>
2026-01-12 11:25:10 -0300

    wtf

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,15 +13,15 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
+    
+    
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+     console.log('Fetching revenue data...');
+     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+     console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -183,6 +183,8 @@ export async function fetchCustomers() {
     throw new Error('Failed to fetch all customers.');
   }
 }
+
+
 
 export async function fetchFilteredCustomers(query: string) {
   try {
